52049d1d1ac41ab1ca25e7d5d95052b566f09fe0
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-08-17 15:15:54 -0300

    Implementando repositórios do Prisma

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,18 @@
+import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+
+import { DatabaseModule } from '../database/database.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { Module } from '@nestjs/common'
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+})
+export class HttpModule {}
