1f6f540dc5683bdd1dd0776cede5b4afab87bb48
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-08-18 13:21:38 -0300

    Listando perguntas recentes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -6,10 +6,11 @@ import { DatabaseModule } from '../database/database.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { Module } from '@nestjs/common'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 @Module({
   imports: [DatabaseModule],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
   controllers: [
     CreateAccountController,
     AuthenticateController,
