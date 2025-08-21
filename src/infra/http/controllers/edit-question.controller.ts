9441dc7b458392a9ed211fdd17d796e31bd84a7b
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-08-21 15:13:06 -0300

    Deletar pergunta

--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -1,10 +1,10 @@
 import {
-    BadRequestException,
-    Body,
-    Controller,
-    HttpCode,
-    Param,
-    Put,
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
 } from '@nestjs/common'
 import { CurrentUser } from '@/infra/auth/current-user-decorator'
 import { UserPayload } from '@/infra/auth/jwt.strategy'
@@ -13,8 +13,8 @@ import { z } from 'zod'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
 
 const editQuestionBodySchema = z.object({
-    title: z.string(),
-    content: z.string(),
+  title: z.string(),
+  content: z.string(),
 })
 
 const bodyValidationPipe = new ZodValidationPipe(editQuestionBodySchema)
@@ -23,28 +23,28 @@ type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>
 
 @Controller('/questions/:id')
 export class EditQuestionController {
-    constructor(private editQuestion: EditQuestionUseCase) { }
+  constructor(private editQuestion: EditQuestionUseCase) {}
 
-    @Put()
-    @HttpCode(204)
-    async handle(
-        @Body(bodyValidationPipe) body: EditQuestionBodySchema,
-        @CurrentUser() user: UserPayload,
-        @Param('id') questionId: string,
-    ) {
-        const { title, content } = body
-        const userId = user.sub
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @Body(bodyValidationPipe) body: EditQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('id') questionId: string,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
 
-        const result = await this.editQuestion.execute({
-            title,
-            content,
-            authorId: userId,
-            attachmentsIds: [],
-            questionId,
-        })
+    const result = await this.editQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
+      questionId,
+    })
 
-        if (result.isLeft()) {
-            throw new BadRequestException()
-        }
+    if (result.isLeft()) {
+      throw new BadRequestException()
     }
+  }
 }
