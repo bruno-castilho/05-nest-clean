6995a8594af3173c95fb9300f8df9837b8f24eab
Bruno da Silva Castilho <bruno.silva.castilho@gmail.com>
2025-08-21 15:42:07 -0300

    Controller: Comentar Pergunta

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -25,6 +25,8 @@ import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-case
 import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller'
 import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
 import { ChooseQuestionBestAnswerUseCase } from '../../domain/forum/application/use-cases/choose-question-best-answer'
+import { CommentOnQuestionController } from './controllers/comment-on-question.controller'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -41,6 +43,7 @@ import { ChooseQuestionBestAnswerUseCase } from '../../domain/forum/application/
     DeleteAnswerUseCase,
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
   ],
   controllers: [
     CreateAccountController,
@@ -55,6 +58,7 @@ import { ChooseQuestionBestAnswerUseCase } from '../../domain/forum/application/
     DeleteAnswerController,
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
   ],
 })
 export class HttpModule {}
